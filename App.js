7e311a933ff367090b797988eb71262b54d8884a
Taimoor Ali <taimoorali083@gmail.com>
2023-12-01 22:05:11 +0500

    project 2

--- App.js
+++ App.js
@@ -1,39 +1,54 @@
-import { ImageBackground, View, StyleSheet } from "react-native";
+import { ImageBackground, View, StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
 
 export default function App() {
   return (
+    <SafeAreaView style={styles.main}>
+      <ImageBackground
+        source={require('./assets/images.png')}
+        style={styles.image}
+        resizeMode="contain"
+      >
+        <View style={styles.container} />
+        <View style={styles.bottomBtnOne} />
+      </ImageBackground>
 
-    <ImageBackground
-      source={require('./assets/images.png')}
-      style={styles.image}
-      resizeMode="cover"
-    >
-      <View
-        style={styles.container}></View>
-      <View style={styles.bottomBtnOne} />
-    </ImageBackground>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    height: 60,
+  main: {
     width: "100%",
+    height: "100%",
+    backgroundColor: "black",
+    display: "flex",
+    justifyContent: "space-evenly",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  container: {
+    height: 50,
+    width: 50,
+    top: 10,
     backgroundColor: "dodgerblue",
-
+    left: 10,
   },
   image: {
     justifyContent: "center",
     width: "100%",
     height: "100%",
     display: "flex",
-    justifyContent: "flex-end"
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    alignContent: "center"
   },
   bottomBtnOne: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "black",
+    height: 50,
+    width: 50,
+    top: 10,
+    position: "absolute",
+    right: 10,
+    backgroundColor: "green",
   },
 })
 
