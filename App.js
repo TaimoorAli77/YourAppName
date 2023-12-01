618ce85ff479c199e3cd77656603a1b6bb2f9098
Taimoor Ali <taimoorali083@gmail.com>
2023-12-01 17:58:07 +0500

    initialially added some react native components

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Image, StyleSheet, Text, View, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  // const pressFunc = () => {
+  //   console.log("Taimooor Clicked Me")
+  // }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text> Welcome Taimoor Ali 2</Text>
+      {/* <Text numberOfLines={1} onPress={() => { pressFunc() }}> Welcome Taimoor Ali 2 </Text> */}
+
+      {/* <Image source={require('./assets/splash.png')} height={10} width={10}></Image> */}
+
+
+
+      <TouchableHighlight onPress={() => console.log("Image Taped")}>
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/200/300"
+          }}>
+        </Image>
+      </TouchableHighlight>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
