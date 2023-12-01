370a07de396713061d4b6be629f598a2036710b4
Taimoor Ali <taimoorali083@gmail.com>
2023-12-01 21:16:55 +0500

    done with positions

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         flexDirection: "row",  //horizontal
         justifyContent: "center", //Main axis
         alignItems: "center", // secondary axis
-        flexWrap: "wrap",
+        // flexWrap: "wrap",  // with wrap on positions and shrink don't workout
         alignContent: "center"
 
       }}
@@ -18,12 +18,12 @@ export default function App() {
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 200,
+          width: 100,
           // flexBasis: 100, // as width or height
           height: 100,
           // flex: 1,  // short for flex grow or flex shrink
           // flexShrink: 1
-          flex:-1
+          flex: -1
           // alignSelf:"flex-start"
         }}
       />
@@ -32,7 +32,11 @@ export default function App() {
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 100
+          height: 100,
+          top: 20,
+          right: 20,
+
+          position: "relative"
         }}
       />
 
