043d7e567d5235bb32e436654dad6d43ae67b7b1
Taimoor Ali <taimoorali083@gmail.com>
2023-12-02 19:53:07 +0500

    pjct 2 done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { ImageBackground, View, StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
-
+import colors from './colors.js'
 export default function App() {
   return (
     <SafeAreaView style={styles.main}>
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   main: {
     width: "100%",
     height: "100%",
-    backgroundColor: "black",
+    backgroundColor: colors.dark,
     display: "flex",
     justifyContent: "space-evenly",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     top: 10,
-    backgroundColor: "dodgerblue",
+    backgroundColor: colors.primary,
     left: 10,
   },
   image: {
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     top: 10,
     position: "absolute",
     right: 10,
-    backgroundColor: "green",
+    backgroundColor: colors.secondary,
   },
 })
 
